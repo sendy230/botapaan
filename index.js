bcaf276a72fe162251e47b67bc224d5a9587a041
BimoSora <bimosora3@gmail.com>
2021-07-17 22:48:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -208,10 +208,10 @@ bot.start(async(ctx)=>{
                         //saving user details to the database
                         saver.saveUser(user)
                     }
-                }}}}) 
+                }
             catch(error){
                 ctx.reply(`${messagebotnoaddgroup2}`)
-            }
+            }}}}) 
         }
     }
 })
