74ff977d1b747a25e3838cf5b96dba821b1a768d
BimoSora <bimosora3@gmail.com>
2021-07-18 18:46:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -231,9 +231,9 @@ bot.action('DOC',(ctx)=>{
 })
 
 bot.command('reload',async(ctx)=>{
-    var adminstatus = await bot.telegram.getChatMember(channelId, ctx.from.id)
+    var adminstatus = await bot.telegram.getChatAdministrator(channelId, ctx.from.id)
     console.log(adminstatus);
-    if((adminstatus.status == 'creator') || (adminstatus.status == 'administrator')){
+    if(adminstatus.status == 'creator' || adminstatus.status == 'administrator'){
        ctx.reply('BOT di mulai ulang')
     }else{
        ctx.reply('Anda bukan Admin')
