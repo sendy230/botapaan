6d31c8e34f3720d54d9dc28f8fc32fd9a7b272af
BimoSora <bimosora3@gmail.com>
2021-07-18 17:33:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -230,16 +230,15 @@ bot.action('DOC',(ctx)=>{
     })
 })
 
-bot.command('reload', (ctx) => {
-    bot.getChatMember(ctx.chat.id, ctx.from.id).then(function(data) {
-        if ((data.status == "creator") || (data.status == "administrator")){
-	    bot.sendMessage(ctx.chat.id, "BOT direload");
-	}else{
-	    bot.sendMessage(ctx.chat.id, "Hanya Admin yang bisa akses");
-	}
-    });
+bot.command(reload, function(message) {
+	bot.getChatMember(message.chat.id, message.from.id).then(function(data) {
+		if ((data.status == "creator") || (data.status == "administrator")){
+			bot.sendMessage(message.chat.id, "I'm admin!");
+		}else{
+			bot.sendMessage(message.chat.id, "I'm not admin");
+		}
+	});
 });
-
 
 //check account
 bot.command('getid',async(ctx)=>{   
