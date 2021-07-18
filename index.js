c60fd692b4493bae0b5c2e67faa9b637f8233b87
BimoSora <bimosora3@gmail.com>
2021-07-19 06:11:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -235,9 +235,7 @@ bot.command('reload',async(ctx)=>{
     var memberstatus = await bot.telegram.getChatMember(channelId, ctx.from.id)
         console.log(memberstatus);
     if (!memberstatus || memberstatus.status == 'administrator' || memberstatus.status == 'creator'){
-       var anon = await bot.telegram.ChatMember(channelId, ctx.from.id)
-          console.log(anon);
-       if(anon.is_anonymous == 'true'){
+       if(memberstatus.is_anonymous == 'true'){
           ctx.reply('BOT reload')
        }else{
           ctx.reply('BOT reload')
