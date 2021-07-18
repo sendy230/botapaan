7793a92ce6446b33fea66929b82c00ff3906b730
BimoSora <bimosora3@gmail.com>
2021-07-18 07:39:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -222,7 +222,7 @@ bot.action('POP',(ctx)=>{
 //DEFININF CREATEBY CALLBACK
 bot.action('CREATEBY',(ctx)=>{
     var messageby2 = messageby(ctx);
-    ctx.replyWithPhoto(https://images4.imagebam.com/0b/64/89/ME24DG5_o.jpg,{`${messageby2}`,
+    ctx.replyWithPhoto(https://images4.imagebam.com/0b/64/89/ME24DG5_o.jpg,{caption: `${messageby2}`,
         parse_mode:'HTML'  
     })
     ctx.deleteMessage()
