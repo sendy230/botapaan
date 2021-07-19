13c61668370cd47a29fe9ad502556f4c83715352
BimoSora <bimosora3@gmail.com>
2021-07-19 21:08:28 +0700

    first commit

--- index.js
+++ index.js
@@ -796,14 +796,14 @@ bot.on('inline_query',async(ctx)=>{
     if(query.length>0){
         let searchResult = saver.getfileInline(query).then((res)=>{
             let result = res.map((ctx,index)=>{
-                var index;
+                var index2;
                 var a = ["document", "video"];
-                for (index = 0; index < a.length; ++index) {
+                for (index2 = 0; index2 < a.length; ++index2) {
                     return {
-                        type:a[index],
+                        type:a[index2],
                         id:ctx._id,
                         title:ctx.file_name,
-                        a[index]_file_id:ctx.file_id,
+                        a[index2]_file_id:ctx.file_id,
                         caption:ctx.caption,
                         reply_markup:{
                             inline_keyboard:[
