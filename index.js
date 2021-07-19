97d7efdb94a0ec43a80fe89b79fc9888dcc564a3
BimoSora <bimosora3@gmail.com>
2021-07-19 13:42:01 +0700

    first commit

--- index.js
+++ index.js
@@ -795,37 +795,20 @@ bot.on('inline_query',async(ctx)=>{
     query = ctx.inlineQuery.query
     if(query.length>0){
         let searchResult = saver.getfileInline(query).then((res)=>{
-            var result =
-            [
-                res.map((ctx,index)=>{
-                    return {
-                        type:'document',
-                        id:ctx._id,
-                        title:ctx.file_name,
-                        document_file_id:ctx.file_id,
-                        caption:ctx.caption,
-                        reply_markup:{
-                            inline_keyboard:[
-                                [{text:"Pencarian",switch_inline_query:''}]
-                            ]
-                        }
+            let result = res.map((ctx,index)=>{
+                return {
+                    type:'document',
+                    id:ctx._id,
+                    title:ctx.file_name,
+                    document_file_id:ctx.file_id,
+                    caption:ctx.caption,
+                    reply_markup:{
+                        inline_keyboard:[
+                            [{text:"Pencarian",switch_inline_query:''}]
+                        ]
                     }
-                }),
-                res.map((ctx,index)=>{
-                    return {
-                        type:'video',
-                        id:ctx._id,
-                        title:ctx.file_name,
-                        document_file_id:ctx.file_id,
-                        caption:ctx.caption,
-                        reply_markup:{
-                            inline_keyboard:[
-                                [{text:"Pencarian",switch_inline_query:''}]
-                            ]
-                        }
-                    }
-                })
-            ]
+                }
+            })
             ctx.answerInlineQuery(result)
         })
     }else{
