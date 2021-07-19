8e1698898131d35affb1dc936a131289c2f716ba
BimoSora <bimosora3@gmail.com>
2021-07-19 23:15:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -796,14 +796,14 @@ bot.on('inline_query',async(ctx)=>{
     if(query.length>0){
         let searchResult = saver.getfileInline(query).then((res)=>{
             let result = res.map((ctx,index)=>{
-                const myArray = ['document','video','audio']
+                var myArray = ['document','video','audio']
                 for (let i = 0; i < myArray.length; i++) {
                     console.log(myArray);
                     return {
                         type:myArray[i],
                         id:ctx._id,
                         title:ctx.file_name,
-                        document_file_id:ctx.file_id,
+                        myArray[i]…_file_id:ctx.file_id,
                         caption:ctx.caption,
                         reply_markup:{
                             inline_keyboard:[
