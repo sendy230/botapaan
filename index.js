61d7fe87040977124794041382d673d46d6ed4b2
BimoSora <bimosora3@gmail.com>
2021-07-19 11:10:40 +0700

    first commit

--- index.js
+++ index.js
@@ -793,7 +793,7 @@ bot.command('stats',async(ctx)=>{
 //getting files as inline result
 bot.on('inline_query',async(ctx)=>{
     query = ctx.inlineQuery.query
-    if(ctx.chat.type == 'private') {
+    
         if(query.length>0){
             let searchResult = saver.getfileInline(query).then((res)=>{
                 let result = res.map((item,index)=>{
@@ -816,7 +816,7 @@ bot.on('inline_query',async(ctx)=>{
         }else{
             console.log('query not found');
         }
-    }
+
     
 })
 
