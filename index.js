be9be205ff401db66aaa0a28b90675b9fa724517
BimoSora <bimosora3@gmail.com>
2021-07-20 10:06:54 +0700

    first commit

--- index.js
+++ index.js
@@ -796,16 +796,31 @@ bot.on('inline_query',async(ctx)=>{
     if(query.length>0){
         let searchResult = saver.getfileInline(query).then((res)=>{
             let result = res.map((ctx,index)=>{
-                return {
-                    type:'document',
-                    id:ctx._id,
-                    title:ctx.file_name,
-                    document_file_id:ctx.file_id,
-                    caption:ctx.caption,
-                    reply_markup:{
-                        inline_keyboard:[
-                            [{text:"Pencarian",switch_inline_query:''}]
-                        ]
+                if(ctx.type == 'document'){
+                    return {
+                        type:'document',
+                        id:ctx._id,
+                        title:ctx.file_name,
+                        document_file_id:ctx.file_id,
+                        caption:ctx.caption,
+                        reply_markup:{
+                            inline_keyboard:[
+                                [{text:"Pencarian",switch_inline_query:''}]
+                            ]
+                        }
+                    }
+                }else{
+                    return {
+                        type:'video',
+                        id:ctx._id,
+                        title:ctx.file_name,
+                        video_file_id:ctx.file_id,
+                        caption:ctx.caption,
+                        reply_markup:{
+                            inline_keyboard:[
+                                [{text:"Pencarian",switch_inline_query:''}]
+                            ]
+                        }
                     }
                 }
             })    
