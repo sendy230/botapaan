8cee4a8949c7bd8e5b946da0a86fb70c7f301084
BimoSora <bimosora3@gmail.com>
2021-07-20 08:59:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -796,28 +796,25 @@ bot.on('inline_query',async(ctx)=>{
     if(query.length>0){
         let searchResult = saver.getfileInline(query).then((res)=>{
             let result = res.map((ctx,index)=>{
-                 myArray.push('{
-                        type:document,
+                let myArray = ['document','video']
+                let myArray2 =[];
+                for (let i = 0; i < myArray.length; i++) {
+                    console.log(myArray)
+                    myArray.push[i]
+                    myArray2.push(myArray.concat())
+                      return {
+                        type:myArray2,
                         id:ctx._id,
                         title:ctx.file_name,
-                        document_file_id:ctx.file_id,
+                        myArray2_file_id:ctx.file_id,
                         caption:ctx.caption,
                         reply_markup:{
                             inline_keyboard:[
                                 [{text:"Pencarian",switch_inline_query:''}]
                             ]
-                        }','{
-                        type:video,
-                        id:ctx._id,
-                        title:ctx.file_name,
-                        video_file_id:ctx.file_id,
-                        caption:ctx.caption,
-                        reply_markup:{
-                            inline_keyboard:[
-                                [{text:"Pencarian",switch_inline_query:''}]
-                            ]
-                        }');
-                 return myArray
+                        }
+                    }
+                }
             })
             ctx.answerInlineQuery(result)
         })
