19bc40052f612290b487c66234ab90a348d8b84b
BimoSora <bimosora3@gmail.com>
2021-07-20 15:18:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -811,7 +811,7 @@ bot.on('inline_query',async(ctx)=>{
                         type:ctx.type,
                         id:ctx._id,
                         title:ctx.file_name,
-                        [`${ctx.type}_file_id`] = ctx.file_id;
+                        [`${ctx.type}_file_id`] = ctx.file_id,
                         caption:ctx.caption,
                         reply_markup:{
                             inline_keyboard:[
