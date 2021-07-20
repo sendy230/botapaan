d470827e7a5041b659929511fd1fad5177e05ddb
BimoSora <bimosora3@gmail.com>
2021-07-20 10:08:13 +0700

    first commit

--- index.js
+++ index.js
@@ -796,7 +796,7 @@ bot.on('inline_query',async(ctx)=>{
     if(query.length>0){
         let searchResult = saver.getfileInline(query).then((res)=>{
             let result = res.map((ctx,index)=>{
-                if(ctx.type == 'document'){
+                if(!type || type == 'document'){
                     return {
                         type:'document',
                         id:ctx._id,
