e88cb4c971d608e0328acd61044a52cb13728b29
BimoSora <bimosora3@gmail.com>
2021-07-21 13:38:46 +0700

    first commit

--- index.js
+++ index.js
@@ -22,7 +22,10 @@ var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
     var yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    today = mm + '/' + dd + '/' + yyyy + '' + hours + ':' + minutes + ':' + seconds;
 
 //Function
 function first_name2(ctx){
