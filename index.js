f505778642f4048b7a5f83b5590df63eb926e169
BimoSora <bimosora3@gmail.com>
2021-07-21 10:41:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ db.connect((err) => {
 //ID Channel/Group
 const channelId = -1001221419739;
 
-let date_ob = new Date(ctx.message.date * 1000);
+let ts = Date.now();
+let date_ob = new Date(ts);
 // current date
 // adjust 0 before single digit date
 let date = ("0" + date_ob.getDate()).slice(-2);
