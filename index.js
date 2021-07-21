3987cd17eba291bcf57b00a3707aa2657abe0e18
BimoSora <bimosora3@gmail.com>
2021-07-21 10:53:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,17 +21,11 @@ const channelId = -1001221419739;
 let ts = Date.now();
 let date_ob = new Date(ts);
 // current date
-// adjust 0 before single digit date
-let date = ("0" + date_ob.getDate()).slice(-2);
-// current month
-let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-// current year
+let date = date_ob.getDate();
+let month = date_ob.getMonth() + 1;
 let year = date_ob.getFullYear();
-// current hours
 let hours = date_ob.getHours();
-// current minutes
 let minutes = date_ob.getMinutes();
-// current seconds
 let seconds = date_ob.getSeconds();
 
 //result date
