49fbaf9394671d9357937b19b5cf0cea60e1f8c9
BimoSora <bimosora3@gmail.com>
2021-07-23 15:23:30 +0700

    first commit

--- index.js
+++ index.js
@@ -256,13 +256,14 @@ bot.command('ban',async(ctx)=>{
     var botStatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id)
     await bot.telegram.getChatMember(ctx.chat.id, ctx.from.id).then(function(data2) {
         console.log(data2);
-    if(ctx.chat.type == 'supergroup') {
-        if (data2.status == 'creator' || data2.status == 'administrator' || data2.status == 'left'){
-            await bot.telegram.kickChatMember(ctx.chat.id, ctx.reply_to_message.from.id).then(result => {
-				await bot.telegram.sendMessage(ctx.chat.id, ctx.reply_to_message.from.username + " melanggar!");
-			})
+        if(ctx.chat.type == 'supergroup') {
+            if (data2.status == 'creator' || data2.status == 'administrator' || data2.status == 'left'){
+                await bot.telegram.kickChatMember(ctx.chat.id, ctx.reply_to_message.from.id).then(result => {
+                    await bot.telegram.sendMessage(ctx.chat.id, ctx.reply_to_message.from.username + " melanggar!");
+                })
+            }
         }
-    }
+    })
 })
 //end
 
