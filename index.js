0b644e68cb6d9143a4b18142ca8203e39a921b56
BimoSora <bimosora3@gmail.com>
2021-07-23 19:24:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -258,9 +258,9 @@ bot.command('ban',async(ctx)=>{
         console.log(memberstatus)
     if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
         if (memberstatus.status == 'creator' || memberstatus.status == 'administrator' || memberstatus.status == 'left'){
-            await bot.telegram.kickChatMember(ctx.chat.id, ctx.reply_to_message.from.id).then(result => {
+            await bot.telegram.banChatMember(ctx.chat.id, ctx.reply_to_message.from.id).then(result => {
                console.log(result)
-               bot.telegram.sendMessage(ctx.chat.id, ctx.reply_to_message.from.username + " melanggar!")
+               ctx.reply(ctx.chat.id, ctx.reply_to_message.from.username + " melanggar!")
             })
         }
     }
