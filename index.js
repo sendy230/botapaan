d42b2c17e82d80dd99798c6bd7878883a69ffe96
BimoSora <bimosora3@gmail.com>
2021-07-23 12:21:43 +0700

    first commit

--- index.js
+++ index.js
@@ -261,9 +261,24 @@ bot.command('ban', async(ctx)=>{
 	var username2 = ctx.reply_to_message.from.username;
 	var userid2 = ctx.reply_to_message.from.id;
 	await bot.getChatMember(ctx.chat.id, ctx.from.id).then(function(data) {
-		if ((data.status == 'creator') || (data.status == 'administrator' || data.status == 'left')){
+		if ((data.status == 'creator') || (data.status == 'administrator') || (data.status == 'left')){
 			await bot.kickChatMember(ctx.chat.id, userid2).then(result => {
 				await bot.sendMessage(ctx.chat.id, username2 + " melanggar peraturan!")
+			})
+		}
+	})
+})
+
+bot.command('unban', async(ctx)=>{
+	if (message.reply_to_message == undefined){
+		return;
+	}
+	var username2 = ctx.reply_to_message.from.username;
+	var userid2 = ctx.reply_to_message.from.id;
+	await bot.getChatMember(ctx.chat.id, ctx.from.id).then(function(data) {
+		if ((data.status == 'creator') || (data.status == 'administrator') || (data.status == 'left')){
+			await bot.unbanChatMember(ctx.chat.id, userid2).then(result => {
+				await bot.sendMessage(ctx.chat.id, ctx.from.first_name + " blokir telah dibuka!")
 			})
 		}
 	})
