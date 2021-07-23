7794ce0d0cabf4d9aece00bf630b1bc37e47efdd
BimoSora <bimosora3@gmail.com>
2021-07-23 13:54:58 +0700

    first commit

--- index.js
+++ index.js
@@ -253,10 +253,6 @@ bot.command('reload',async(ctx)=>{
 })
 
 bot.command('ban',function(ctx){
-    if (ctx.reply_to_message == undefined){
-		// Not used via reply
-		return;
-	}
 	var username2 = ctx.reply_to_message.from.username;
 	var userid2 = ctx.reply_to_message.from.id;
     var botStatus = bot.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id)
