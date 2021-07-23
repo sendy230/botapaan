103340fd4c0eb36108da4ef9190cc2842f6da9a1
BimoSora <bimosora3@gmail.com>
2021-07-23 14:06:06 +0700

    first commit

--- index.js
+++ index.js
@@ -252,14 +252,14 @@ bot.command('reload',async(ctx)=>{
     }
 })
 
-bot.command('ban',function(ctx){
+bot.command('banned',function(ctx){
     var botStatus = bot.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id)
     bot.telegram.getChatMember(ctx.chat.id, ctx.from.id).then(function(data2) {
             console.log(memberstatus);
         if(ctx.chat.type == 'supergroup') {
             if ((data2.status == 'creator') || (data2.status == 'administrator') || (data2.status == 'left')){
                 bot.telegram.kickChatMember(ctx.chat.id, ctx.reply_to_message.from.id).then(result => {
-                    ctx.reply(ctx.chat.id, ctx.reply_to_message.from.username + ' banned!')
+                    bot.telegram.sendMessage(ctx.chat.id, ctx.reply_to_message.from.username + ' banned!')
                 })
             }
         }
