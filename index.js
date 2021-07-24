17b499ae7830c72c0393bb97f6f052e6a20e2b5b
BimoSora <bimosora3@gmail.com>
2021-07-24 21:08:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -264,6 +264,34 @@ bot.command('kick',async(ctx)=>{
         }
     }
 })
+
+bot.command('ban',async(ctx)=>{
+    var botStatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id)
+    var memberstatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.from.id)
+        console.log(memberstatus);
+        if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
+        if (!memberstatus || memberstatus.status == 'creator' || memberstatus.status == 'administrator' || memberstatus.status == 'left'){
+            await bot.telegram.banChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id).then(result=>{
+                 console.log(result)
+                 ctx.reply(`Melanggar peraturan grup!`)
+            })
+        }
+    }
+})
+
+bot.command('unban',async(ctx)=>{
+    var botStatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id)
+    var memberstatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.from.id)
+        console.log(memberstatus);
+        if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
+        if (!memberstatus || memberstatus.status == 'creator' || memberstatus.status == 'administrator' || memberstatus.status == 'left'){
+            await bot.telegram.unbanChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id).then(result=>{
+                 console.log(result)
+                 ctx.reply(`Pengguna tidak diblokir, boleh masuk kembali!`)
+            })
+        }
+    }
+})
 //END
 
 //check account
