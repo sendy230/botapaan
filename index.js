c8bd88c8988260bc9b3618f5107abf04af637591
BimoSora <bimosora3@gmail.com>
2021-07-24 09:33:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -252,15 +252,15 @@ bot.command('reload',async(ctx)=>{
     }
 })
 
-bot.on('ban',async(message)=>{    
-    var botStatus = await bot.getChatMember(message.chat.id, message.botInfo.id)
-    var memberstatus = await bot.getChatMember(message.chat.id, message.from.id)
+bot.on('ban',async(ctx)=>{    
+    var botStatus = await bot.getChatMember(ctx.chat.id, ctx.botInfo.id)
+    var memberstatus = await bot.getChatMember(ctx.chat.id, ctx.from.id)
         console.log(memberstatus)
     if(message.chat.type == 'group' || message.chat.type == 'supergroup') {
         if (memberstatus.status == 'creator' || memberstatus.status == 'administrator' || memberstatus.status == 'left'){
-            await bot.banChatMember(message.chat.id, message.reply_to_message.from.id).then(result => {
+            await bot.banChatMember(ctx.chat.id, ctx.reply_to_message.from.id).then(result => {
                console.log(result)
-               message.reply(message.chat.id, message.reply_to_message.from.username + " melanggar!")
+               ctx.reply(ctx.chat.id, ctx.reply_to_message.from.username + " melanggar!")
             })
         }
     }
