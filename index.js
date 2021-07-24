fa28f9cfaf56b74843532d2d519baeabd7775f2e
BimoSora <bimosora3@gmail.com>
2021-07-25 00:09:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -257,6 +257,7 @@ bot.command('kick',async(ctx)=>{
     var memberstatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.from.id)
         console.log(memberstatus);
         if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
+        let args = ctx.message.text.split(" ").slice(1)
         if (!memberstatus || memberstatus.status == 'creator' || memberstatus.status == 'administrator' || memberstatus.status == 'left'){
             await bot.telegram.kickChatMember(ctx.chat.id, args[0]).then(result=>{
                  console.log(result)
