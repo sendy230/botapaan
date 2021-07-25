9f260eea9e0cfd822c2a422c475f8b1e5f4f1a77
BimoSora <bimosora3@gmail.com>
2021-07-25 12:39:08 +0700

    first commit

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ bot.command('ban',async(ctx)=>{
                 console.log(result)
                 ctx.reply(`Melanggar peraturan grup!`)
                 if(ctx.chat.type == 'private') {
-                    bot.telegram.sendMessage(ctx.message.chat.id, ctx.message.reply_to_message.from.username + ` melanggar peraturan grup!`)
+                    bot.telegram.sendMessage(ctx.message.chat.id, ctx.message.reply_to_message.from.from.id + ` melanggar peraturan grup!`)
                 }
             })
         }
