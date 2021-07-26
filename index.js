48f9a9c1a81c806ffcc5312ab4243eca5bf97c93
BimoSora <bimosora3@gmail.com>
2021-07-26 07:53:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -240,15 +240,15 @@ bot.action('DOC',(ctx)=>{
     })
 })
 
-bot.hears('ping', (ctx)=> {
-   let chatId = ctx.message.chat.id;
+bot.hears('ping', (ctx)=>{
+   let chatId = ctx.message.from.id;
      let opts = {
         reply_to_message_id: ctx.message.message_id,
         reply_markup: JSON.stringify({
         keyboard: [[{text: 'OK'}]],
      }),
    }
-   bot.sendMessage(chatId, 'pong', opts);
+   bot.telegram.sendMessage(chatId, 'pong', opts);
 })
 
 //GROUP COMMAND
