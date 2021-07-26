798d858c831e0fb5e22c43b437551e821fa8b31c
BimoSora <bimosora3@gmail.com>
2021-07-26 12:45:20 +0700

    first commit

--- help.js
+++ help.js
@@ -4,30 +4,30 @@ module.exports = `<u>Berikut adalah beberapa perintah dan penggunaan admin.</u>
 \n<b>/unban</b> userID.
 \n<b>/kick</b> userID.
 \n<b>(Dapatkan UserID dari saluran log).</b>
-
-\n\n<b>Cara Menghapus File Dari Bot.</b>
+---->
+\n<b>Cara Menghapus File Dari Bot.</b>
 \nAnda dapat menghapus file 3 cara.
 \n⚫️ Hapus file individual dengan file_id.
 \n⚫️ Hapus semua file Kirim oleh pengguna.
 \n⚫️ Hapus semua file Kirim ke Bot.
-
-\n\n<code>Hapus file individual dengan file_id.</code>
+---->
+\n<code>Hapus file individual dengan file_id.</code>
 \n<b>/rem</b> file_id.
 \n<b>(Ini akan menghapus file satu per satu saat Anda memberikan file_id, dapatkan file_id dari saluran log).</b>
-
-\n\n<code>Hapus semua file Kirim oleh pengguna.</code>
+---->
+\n<code>Hapus semua file Kirim oleh pengguna.</code>
 \n<b>/remall</b> userID.
 \n<b>(Anda dapat menghapus semua file dikirim oleh pengguna tertentu jika pengguna mengirim konten atau spam yang kasar, dapatkan userid dari saluran log).</b>
-
-\n\n<code>Hapus semua file Kirim ke B0T.</code>
+---->
+\n<code>Hapus semua file Kirim ke B0T.</code>
 \n<b>/clear</b>
 \n<b>(Ini akan menghapus semua file yang dikirim ke bot secara permanen).</b>
-
-\n\n<code>Kirim pesan ke pengguna.</code>
+---->
+\n<code>Kirim pesan ke pengguna.</code>
 \n<b>/send</b> Pesan Anda akan dikirim ke pengguna.
 \n<b>(Anda dapat menyiarkan pesan teks ke pengguna Anda, pesan akan dikirim dari pengguna terakhir bergabung untuk pertama-tama bergabung dengan pengguna untuk mengurangi spam. Cobalah untuk tidak mengirim terlalu banyak pesan sekaligus jika Anda memiliki sejumlah besar pengguna).</b>
-
-\n\n<code>Cara Mengetahui Total Pengguna Bot.</code>
+---->
+\n<code>Cara Mengetahui Total Pengguna Bot.</code>
 \n<b>/stats</b>
 \n<b>(Anda akan mendapatkan total pengguna memulai bot Anda, data waktu nyata akan diperbarui setelah siaran yang berhasil).</b>
 `;
