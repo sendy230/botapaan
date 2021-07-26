4d6d30fa3fccaf85d836969a38ab429fffdd9055
BimoSora <bimosora3@gmail.com>
2021-07-26 14:53:25 +0700

    first commit

--- help.js
+++ help.js
@@ -1,41 +1,21 @@
 module.exports.bothelp = `Halo, kami adalah bot File Saver. Kami akan terus memperbarui BOT kami, jika kalian menyukainya silakan instal dan ikuti langkahnya.`;
 
 module.exports.botcommand = `<u>Berikut adalah beberapa perintah dan penggunaan admin.</u>
-\n\n<code>Bagaimana pengguna melarang, unban dan kick dari BOT dan Grup.</code>
-\n<b>/ban</b> userID.
-\n<b>/unban</b> userID.
-\n<b>/kick</b> userID.
-\n<b>(Dapatkan UserID dari saluran log).</b>
+\n\n<code>Bagaimana pengguna melarang, unban dan kick dari BOT dan Grup.</code>\n<b>/ban</b> userID.\n<b>/unban</b> userID.\n<b>/kick</b> userID.\n<b>(Dapatkan UserID dari saluran log).</b>
 
-\nBagaimana cara menggunakan pin dan unpin di grup.
-\n<b>/pin</b> reply ke pesan yang mau di pin.
-\n<b>/unpin</b> reply ke pesan yang mau di unpin.
+\nBagaimana cara menggunakan pin dan unpin di grup.\n<b>/pin</b> reply ke pesan yang mau di pin.\n<b>/unpin</b> reply ke pesan yang mau di unpin.
 
-\n<b>Cara Menghapus File Dari Bot.</b>
-\nAnda dapat menghapus file 3 cara.
-\n⚫️ Hapus file individual dengan file_id.
-\n⚫️ Hapus semua file Kirim oleh pengguna.
-\n⚫️ Hapus semua file Kirim ke Bot.
+\n<b>Cara Menghapus File Dari Bot.</b>\nAnda dapat menghapus file 3 cara.\n⚫️ Hapus file individual dengan file_id.\n⚫️ Hapus semua file Kirim oleh pengguna.\n⚫️ Hapus semua file Kirim ke Bot.
 
-\n<code>Hapus file individual dengan file_id.</code>
-\n<b>/rem</b> file_id.
-\n<b>(Ini akan menghapus file satu per satu saat Anda memberikan file_id, dapatkan file_id dari saluran log).</b>
+\n<code>Hapus file individual dengan file_id.</code>\n<b>/rem</b> file_id.\n<b>(Ini akan menghapus file satu per satu saat Anda memberikan file_id, dapatkan file_id dari saluran log).</b>
 
-\n<code>Hapus semua file Kirim oleh pengguna.</code>
-\n<b>/remall</b> userID.
-\n<b>(Anda dapat menghapus semua file dikirim oleh pengguna tertentu jika pengguna mengirim konten atau spam yang kasar, dapatkan userid dari saluran log).</b>
+\n<code>Hapus semua file Kirim oleh pengguna.</code>\n<b>/remall</b> userID.\n<b>(Anda dapat menghapus semua file dikirim oleh pengguna tertentu jika pengguna mengirim konten atau spam yang kasar, dapatkan userid dari saluran log).</b>
 
-\n<code>Hapus semua file Kirim ke B0T.</code>
-\n<b>/clear</b>
-\n<b>(Ini akan menghapus semua file yang dikirim ke bot secara permanen).</b>
+\n<code>Hapus semua file Kirim ke B0T.</code>\n<b>/clear</b>\n<b>(Ini akan menghapus semua file yang dikirim ke bot secara permanen).</b>
 
-\n<b>Kirim pesan ke pengguna.</b>
-\n<b>/send</b> Pesan Anda akan dikirim ke pengguna.
-\n<b>(Anda dapat menyiarkan pesan teks ke pengguna Anda, pesan akan dikirim dari pengguna terakhir bergabung untuk pertama-tama bergabung dengan pengguna untuk mengurangi spam. Cobalah untuk tidak mengirim terlalu banyak pesan sekaligus jika Anda memiliki sejumlah besar pengguna).</b>
+\n<b>Kirim pesan ke pengguna.</b>\n<b>/send</b> Pesan Anda akan dikirim ke pengguna.\n<b>(Anda dapat menyiarkan pesan teks ke pengguna Anda, pesan akan dikirim dari pengguna terakhir bergabung untuk pertama-tama bergabung dengan pengguna untuk mengurangi spam. Cobalah untuk tidak mengirim terlalu banyak pesan sekaligus jika Anda memiliki sejumlah besar pengguna).</b>
 
-\n<b>Cara Mengetahui Total Pengguna Bot.</b>
-\n<b>/stats</b>
-\n<b>(Anda akan mendapatkan total pengguna memulai bot Anda, data waktu nyata akan diperbarui setelah siaran yang berhasil).</b>
+\n<b>Cara Mengetahui Total Pengguna Bot.</b>\n<b>/stats</b>\n<b>(Anda akan mendapatkan total pengguna memulai bot Anda, data waktu nyata akan diperbarui setelah siaran yang berhasil).</b>
 `;
 
 module.exports.botinstall = `BOT akan memberitahu Anda cara install
