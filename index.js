5e3f90a5fac22a8332e6a238dc800d19c58279ad
BimoSora <bimosora3@gmail.com>
2021-07-27 01:27:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -749,7 +749,7 @@ bot.on('video', async(ctx) => {
            type: 'video'
        }
        console.log(fileDetails2.caption);
-    }else{
+    }else if(fileDetails0.file_name == defined){
        var exstension = video.file_name;
        var regex = /.(webm|mkv|flv|vob|ogv|ogg|drc|gif|gifv|mng|avi|mts|m2ts|ts|mov|qt|wmv|yuv|rm|rmvb|viv|asf|amv|mp4|m4p|m4v|mpg|mp2|mpeg|mpe|mpv|mpg|mpeg|m2v|m4v|svi|3gp|3g2|mxf|roq|nsv|flv|f4v|f4p|f4a|f4b)/i;
        var vidext = exstension.replace(regex, '');
