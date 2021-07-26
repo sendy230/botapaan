eccc298327cabf08fb22e78eb3ba2197228df0a9
BimoSora <bimosora3@gmail.com>
2021-07-27 00:31:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -726,7 +726,7 @@ bot.on('video', async(ctx) => {
     var exstension = video.file_name;
     var regex = /.(webm|mkv|flv|vob|ogv|ogg|drc|gif|gifv|mng|avi|mts|m2ts|ts|mov|qt|wmv|yuv|rm|rmvb|viv|asf|amv|mp4|m4p|m4v|mpg|mp2|mpeg|mpe|mpv|mpg|mpeg|m2v|m4v|svi|3gp|3g2|mxf|roq|nsv|flv|f4v|f4p|f4a|f4b)/i;
     var vidext = exstension.replace(regex, '');
-    if(vidext == undefined){
+    if(vidext == ''){
        fileDetails2 = {
            file_name: today2(ctx),
            userId:ctx.from.id,
