52007d24b7c63c12612db09628f0047d0cc409f1
BimoSora <bimosora3@gmail.com>
2021-07-28 16:55:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -451,9 +451,6 @@ bot.command('send',async(ctx)=>{
                 const userId = words.shift();
                 const caption = words.join(" ");
 
-                ctx.reply ('Pesan terkirim!',{
-                    reply_to_message_id: ctx.message.message_id
-                })
                 bot.telegram.sendMessage(userId, `${caption}`)
             }
 
@@ -462,9 +459,6 @@ bot.command('send',async(ctx)=>{
             const command = words.shift().slice(1);
             const caption = words.join(" ");
 
-            ctx.reply ('Pesan terkirim!',{
-                reply_to_message_id: ctx.message.message_id
-            })
             bot.telegram.sendMessage(ctx.message.reply_to_message.from.id, `${caption}`)
         }
     }
