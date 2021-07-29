c741acef41a06c9c4afee02d3d4e00016903cf78
BimoSora <bimosora3@gmail.com>
2021-07-29 13:18:39 +0700

    first commit

--- index.js
+++ index.js
@@ -525,6 +525,35 @@ bot.command('remall', (ctx) => {
     }
 })
 
+// send
+bot.command('sendchat',async(ctx)=>{
+    var botStatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.botInfo.id)
+    var memberstatus = await bot.telegram.getChatMember(ctx.chat.id, ctx.from.id)
+    console.log(memberstatus);
+
+    if(ctx.chat.type == 'private') {
+        if(ctx.from.id ==process.env.ADMIN || ctx.from.id == process.env.ADMIN1 || ctx.from.id == process.env.ADMIN2){
+            const str = ctx.message.text;
+            const words = str.split(/ +/g);
+            const command = words.shift().slice(1);
+            const userId = words.shift();
+            const caption = words.join(" ");
+
+            bot.telegram.sendMessage(userId, `${caption}`)
+        }
+        if (!memberstatus || memberstatus.status == 'creator' || memberstatus.status == 'administrator' || memberstatus.status == 'left'){
+            const str = ctx.message.text;
+            const words = str.split(/ +/g);
+            const command = words.shift().slice(1);
+            const userId = words.shift();
+            const caption = words.join(" ");
+
+            bot.telegram.sendMessage(userId, `${caption}`)
+
+        }
+    }
+})
+
 //broadcasting message to bot users(from last joined to first)
 bot.command('broadcast',async(ctx)=>{
     msg = ctx.message.text
