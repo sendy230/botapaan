7814d40f08219bf6754cf7ff0ce63131e62f143a
BimoSora <bimosora3@gmail.com>
2021-07-29 11:33:52 +0700

    first commit

--- database/filesaver.js
+++ database/filesaver.js
@@ -17,9 +17,16 @@ module.exports={
     getUser:()=>{
         return new Promise(async(resolve,reject)=>{
             db.get().collection(collection.USER_COLLECTION).find().toArray().then((res)=>{
-                db.get().collection(collection. FILE_COLLECTION).find().toArray().then((res)=>{
-                    resolve(res);
-                })
+                resolve(res);
+                
+            })
+        })
+    },
+
+    getMedia:()=>{
+        return new Promise(async(resolve,reject)=>{
+            db.get().collection(collection.FILE_COLLECTION).find().toArray().then((res)=>{
+                resolve(res);
                 
             })
         })
