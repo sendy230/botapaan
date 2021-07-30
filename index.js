790c0a4aaff0e383493330803ec242767d576273
BimoSora <bimosora3@gmail.com>
2021-07-30 07:36:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -692,7 +692,7 @@ bot.command('unbanchat', (ctx) => {
     }
 })
 
-//saving documents to db and generating link
+saving documents to db and generating link
 bot.on('document', async (ctx) => {
     document = ctx.message.document
     console.log(ctx);
@@ -709,7 +709,7 @@ bot.on('document', async (ctx) => {
         console.log(fileDetails1.caption);
     
     if(fileDetails1.file_name == undefined){
-        fileDetails = {
+        fileDetails2 = {
             file_name: today2(ctx),
             userId:ctx.from.id,
             file_id: document.file_id,
@@ -718,7 +718,7 @@ bot.on('document', async (ctx) => {
             uniqueId: document.file_unique_id,
             type: 'document'
         }
-        console.log(fileDetails.caption);
+        console.log(fileDetails2.caption);
     }else{
         fileDetails = {
             file_name: document.file_name,
@@ -996,9 +996,9 @@ bot.on('video', async(ctx) => {
                             })
                     }else{
                         saver.saveFile(fileDetails)
-                        var exstension3 = video.file_name;
-                        var regex3 = /.(webm|mkv|flv|vob|ogv|ogg|drc|gif|gifv|mng|avi|mts|m2ts|ts|mov|qt|wmv|yuv|rm|rmvb|viv|asf|amv|mp4|m4p|m4v|mpg|mp2|mpeg|mpe|mpv|mpg|mpeg|m2v|m4v|svi|3gp|3g2|mxf|roq|nsv|flv|f4v|f4p|f4a|f4b)/i;
-                        var vidext3 = exstension23.replace(regex3, ' ');
+                        var exstension2 = video.file_name;
+                        var regex2 = /.(webm|mkv|flv|vob|ogv|ogg|drc|gif|gifv|mng|avi|mts|m2ts|ts|mov|qt|wmv|yuv|rm|rmvb|viv|asf|amv|mp4|m4p|m4v|mpg|mp2|mpeg|mpe|mpv|mpg|mpeg|m2v|m4v|svi|3gp|3g2|mxf|roq|nsv|flv|f4v|f4p|f4a|f4b)/i;
+                        var vidext3 = exstension2.replace(regex2, ' ');
                         if(ctx.chat.type == 'private') {
                             ctx.reply(`<b>Nama file:</b> ${vidext3}\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_unique_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,{
                                 parse_mode: 'HTML',
