1eb886b49123502861dfd818d528458756256879
BimoSora <bimosora3@gmail.com>
2021-07-31 12:09:24 +0700

    first commit

--- index.js
+++ index.js
@@ -588,7 +588,7 @@ bot.command('sendchat',async(ctx)=>{
                 reply_to_message_id: ctx.message.message_id
             })
             
-            var fromname = ctx.from.username ? `@${ctx.message.from.username}` : `${ctx.message.first_name}`;
+            var fromname = ctx.message.from.username ? `@${ctx.message.from.username}` : `${ctx.message.first_name}`;
             return bot.telegram.sendMessage(userId, `${fromname} menigirm pesan \n${caption}`)
         }
         if (!memberstatus || memberstatus.status == 'creator' || memberstatus.status == 'administrator' || memberstatus.status == 'left'){
@@ -602,7 +602,7 @@ bot.command('sendchat',async(ctx)=>{
                 reply_to_message_id: ctx.message.message_id
             })
 
-            var fromname = ctx.from.username ? `@${ctx.message.from.username}` : `${ctx.message.first_name}`;
+            var fromname = ctx.message.from.username ? `@${ctx.message.from.username}` : `${ctx.message.first_name}`;
             return bot.telegram.sendMessage(userId, `${fromname} menigirm pesan \n${caption}`)
         }
     }
