88a10ae62348cc895682cc0349c59ac6b94dbfbe
BimoSora <bimosora3@gmail.com>
2021-08-01 11:07:39 +0700

    first commit

--- index.js
+++ index.js
@@ -365,23 +365,23 @@ bot.command('kick',async(ctx)=>{
                 var memberstatus = await bot.telegram.getChatMember(group, ctx.from.id)
                 console.log(memberstatus);
 
-                if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
-                    if (!memberstatus || memberstatus.status == 'creator' || memberstatus.status == 'administrator'){             
-                        if (ctx.message.reply_to_message == undefined){
-                            let args = ctx.message.text.split(" ").slice(1)
-                            await bot.telegram.kickChatMember(ctx.chat.id, args[0]).then(result=>{
-                                console.log(result)
-                            })
-                        }
-                        await bot.telegram.kickChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id).then(result=>{
+                if (!memberstatus || memberstatus.status == 'creator' || memberstatus.status == 'administrator'){             
+                    if (ctx.message.reply_to_message == undefined){
+                        let args = ctx.message.text.split(" ").slice(1)
+                        await bot.telegram.kickChatMember(ctx.chat.id, args[0]).then(result=>{
                             console.log(result)
                         })
-                        
                     }
+                    await bot.telegram.kickChatMember(ctx.chat.id, ctx.message.reply_to_message.from.id).then(result=>{
+                        console.log(result)
+                    })
+                        
                 }
             }
         }
-        sendchat()
+        if(ctx.chat.type == 'group' || ctx.chat.type == 'supergroup') {
+            sendchat()
+        }
     })
 })
 
