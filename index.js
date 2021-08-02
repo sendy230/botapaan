797d777003bfaa2fac8e67b0f6f4e8b3f229885b
BimoSora <bimosora3@gmail.com>
2021-08-02 13:13:16 +0700

    first commit

--- index.js
+++ index.js
@@ -1002,13 +1002,13 @@ bot.command('media', (ctx) => {
             {
                 type: "photo",
                 media:{
-                    InputMediaPhoto:"https://www.graphie.co.id/uploads/blog/blog4.jpg"
+                    InputMedia:"https://www.graphie.co.id/uploads/blog/blog4.jpg"
                 }
             },
             {
                 type: "photo",
                 media:{
-                    InputMediaPhoto:"https://www.graphie.co.id/uploads/blog/blog4.jpg"
+                    InputMedia:"https://www.graphie.co.id/uploads/blog/blog4.jpg"
                 }
             }
           
