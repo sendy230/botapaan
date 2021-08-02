9e9889577f8118aa58cc9ccc426c226e2f6c1f68
BimoSora <bimosora3@gmail.com>
2021-08-02 13:34:54 +0700

    first commit

--- index.js
+++ index.js
@@ -995,17 +995,6 @@ bot.command('unbanchat', (ctx) => {
     }
 })
 
-bot.command('media', (ctx) => {
-    if(ctx.chat.type == 'private') {
-        return ctx.replyWithMediaGroup(ctx.chat_id, media=[
-            {
-                "type":"photo",
-                "media":"https://www.graphie.co.id/uploads/blog/blog4.jpg"
-            }
-        ])
-    }
-})
-
 //saving documents to db and generating link
 bot.on('document', async (ctx) => {
     document = ctx.message.document
