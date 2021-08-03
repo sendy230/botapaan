bbf65d2acd0e872c9b4e7ad46a0d26169cf7acb5
BimoSora <bimosora3@gmail.com>
2021-08-03 12:25:12 +0700

    first commit

--- index.js
+++ index.js
@@ -1461,15 +1461,11 @@ bot.on('photo', async(ctx) => {
                             if(!ctx.message.caption)
                             return ctx.telegram.sendMediaGroup(photo[1].file_id, [{
                                 chat_id: process.env.LOG_CHANNEL,
-                                type : 'photo',
-                                media : file_id,
                                 caption: `<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n<b>Nama file:</b> ${today2(ctx)}\n<b>Size:</b> ${photo[1].file_size} B\n<b>ID file:</b> ${photo[1].file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${photo[1].file_unique_id}`,
                                     parse_mode:'HTML'
                             }])
                             ctx.telegram.sendMediaGroup(photo[1].file_id, [{
                                 chat_id: process.env.LOG_CHANNEL,
-                                type : 'photo',
-                                media : file_id,
                                 caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a> \n\n<b>Nama file:</b> ${today2(ctx)}\n<b>Size:</b> ${photo[1].file_size} B\n<b>ID file:</b> ${photo[1].file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${photo[1].file_unique_id}`,
                                     parse_mode:'HTML'
                             }])
