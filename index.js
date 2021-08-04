9543b9f566c2e07db77c9745fd0140f4ec27d3b3
BimoSora <bimosora3@gmail.com>
2021-08-04 20:34:43 +0700

    first commit

--- index.js
+++ index.js
@@ -1169,7 +1169,7 @@ bot.on('video', async(ctx) => {
             console.log(fileDetails.caption);
         }
     }else{
-        if(fileDetails1.mediaId == ctx.message.media_group_id){
+        if( fileDetails3.some(fileDetails3 => fileDetails3.mediaId !== ctx.message.media_group_id)){ 
             var exstension = video.file_name;
             var regex = /\.[A-Za-z0-9]+$/gm
             var vidext = exstension.replace(regex, '');
@@ -1312,16 +1312,11 @@ bot.on('video', async(ctx) => {
                             reply_to_message_id: ctx.message.message_id
                         })
                     }
-                    if(!ctx.message.caption)
+
                     return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
                         type : 'video',
                         media : video.file_id,
-                        caption: `<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
-                            parse_mode:'HTML'
-                    }])
-                    ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
-                        type : 'video',
-                        media : video.file_id,
+                        
                         caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
                             parse_mode:'HTML'
                     }])
