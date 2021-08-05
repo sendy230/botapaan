49203e660802a50fbea7b5c30e49f1937f93dd59
BimoSora <bimosora3@gmail.com>
2021-08-05 12:15:10 +0700

    first commit

--- index.js
+++ index.js
@@ -1310,15 +1310,15 @@ bot.on('video', async(ctx) => {
                         })
                     }
                     let fileDetails3 = []
-                    fileDetails3.push(
-                        media: video.file_id,
-                        caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
-                        parse_mode:'HTML'
+                    fileDetails3.push({
+                            media: video.file_id,
+                            caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
+                            parse_mode:'HTML'
+                        }
                     )
                     return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
                             type: 'video',
                             media: fileDetails3
-
                         }
                     ])
                 })
