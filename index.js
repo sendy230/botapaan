d8bbc71c8e1faff6402acf8afee9dc98270b8737
BimoSora <bimosora3@gmail.com>
2021-08-05 14:10:00 +0700

    first commit

--- index.js
+++ index.js
@@ -1303,17 +1303,16 @@ bot.on('video', async(ctx) => {
                     }
                     saver.saveFile(fileDetails3)
                     if(ctx.chat.type == 'private') {
-                        let text = '';
-                        for (let i = 0; i < 10; i++) {
-                            if (i == 1) {
+                        for (let i = 0; i < 1; i++) {
+                            if (i === 1) {
+                                ctx.reply(`<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${ctx.message.media_group_id}`,{
+                                    parse_mode: 'HTML',
+                                    disable_web_page_preview: true,
+                                    reply_to_message_id: ctx.message.message_id
+                                })
                                 break;
                             }
-                        }
-                        text += ctx.reply(`<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${ctx.message.media_group_id}`,{
-                            parse_mode: 'HTML',
-                            disable_web_page_preview: true,
-                            reply_to_message_id: ctx.message.message_id
-                        })
+                        }                          
                     }
                     if(!ctx.message.caption)
                     return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
