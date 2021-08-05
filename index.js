525f34c47a8cfb809d73eaf87b827a5b4a49955f
BimoSora <bimosora3@gmail.com>
2021-08-05 20:25:59 +0700

    first commit

--- index.js
+++ index.js
@@ -1301,15 +1301,27 @@ bot.on('video', async(ctx) => {
                             }
                         }
                     }
-                    //saver.saveFile(fileDetails3)
-                    let update = []
-                    bot.use((ctx,next)=>{
-                        if(ctx.message.media_group_id){
-                        update.push(ctx.update)
-                        }
-                            next()
-                    })
-                    console.log(bot.use)
+                    saver.saveFile(fileDetails3)
+                    if(ctx.chat.type == 'private') {
+                        ctx.reply(`<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${ctx.message.media_group_id}`,{
+                            parse_mode: 'HTML',
+                            disable_web_page_preview: true,
+                            reply_to_message_id: ctx.message.message_id
+                        })
+                    }
+                    if(!ctx.message.caption)
+                    return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
+                        type : 'video',
+                        media : fileDetails3.file_id,
+                        caption: `<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
+                            parse_mode:'HTML'
+                    }])
+                    ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
+                        type : 'video',
+                        media : fileDetails3.file_id,
+                        caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
+                            parse_mode:'HTML'
+                    }])
                 })
             }
         //}
