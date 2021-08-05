150bbd58ccf04a88eadb2f29796f5e5b0ebeac54
BimoSora <bimosora3@gmail.com>
2021-08-05 11:13:47 +0700

    first commit

--- index.js
+++ index.js
@@ -1309,8 +1309,8 @@ bot.on('video', async(ctx) => {
                             reply_to_message_id: ctx.message.message_id
                         })
                     }
-                    fileDetails3 = []
-                    fileDetails3.append(InputMediaVideo(fileDetails3.file_id,{
+                    media = []
+                    media.append(InputMediaVideo(fileDetails3.file_id,{
                         caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
                         parse_mode:'HTML'
                     }))
