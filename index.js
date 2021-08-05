df768245d63eb8a4f1f4538f0070d4c5526ba724
BimoSora <bimosora3@gmail.com>
2021-08-05 19:50:40 +0700

    first commit

--- index.js
+++ index.js
@@ -1306,11 +1306,11 @@ bot.on('video', async(ctx) => {
                         let update = []
                         bot.use((ctx,next)=>{
                             if(ctx.message.media_group_id){
-                                update.push(ctx.updates)
+                                update.push(ctx.update)
                             }
                             next()
-                            console.log(next())
                         })
+                        console.log(next())
                     }
                 })
             }
