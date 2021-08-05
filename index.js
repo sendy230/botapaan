afc3477635c86f841ce3394cd2a6015ad0451e89
BimoSora <bimosora3@gmail.com>
2021-08-05 11:17:18 +0700

    first commit

--- index.js
+++ index.js
@@ -1310,10 +1310,11 @@ bot.on('video', async(ctx) => {
                         })
                     }
                     fileDetails3 = []
-                    fileDetails3.append(InputMediaVideo(fileDetails3.file_id,{
+                    fileDetails3.append(InputMediaVideo(
+                        media: update.message.video.file_id,
                         caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
                         parse_mode:'HTML'
-                    }))
+                    ))
                     return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
                         type : 'video',
                         media : media
