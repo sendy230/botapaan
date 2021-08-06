0008668816879b67990058e3431c794139a5a42f
BimoSora <bimosora3@gmail.com>
2021-08-06 17:13:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1332,7 +1332,7 @@ bot.on('video', async(ctx) => {
                     }])
                     ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
                         type : 'video',
-                        media : update[0].message.video.file_id,
+                        media : video.file_id,
                         caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
                             parse_mode:'HTML'
                     }])
