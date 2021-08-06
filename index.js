6af84424e9cac7f22d2eaefd1efa097ab0acccd2
BimoSora <bimosora3@gmail.com>
2021-08-06 13:10:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1323,6 +1323,7 @@ bot.on('video', async(ctx) => {
                         }
                     }
                     if(!ctx.message.caption)
+                    if(fileDetails3.mediaId == ctx.message.media_group_id){
                     return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
                         type : 'video',
                         media : video.file_id,
@@ -1335,6 +1336,7 @@ bot.on('video', async(ctx) => {
                         caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
                             parse_mode:'HTML'
                     }])
+                    }
                 })
             }
         //}
