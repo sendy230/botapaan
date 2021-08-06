9612ca1ba939c7367b9519191741f9af43539968
BimoSora <bimosora3@gmail.com>
2021-08-06 12:39:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1322,6 +1322,7 @@ bot.on('video', async(ctx) => {
                             }
                         }
                     }
+                    if(ctx.message.media_group_id){
                     if(!ctx.message.caption)
                     return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
                         type : 'video',
@@ -1335,6 +1336,7 @@ bot.on('video', async(ctx) => {
                         caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
                             parse_mode:'HTML'
                     }])
+                    }
                 })
             }
         //}
