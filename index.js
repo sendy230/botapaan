7f3e969ec8864fe37e0ff19621667adee240b12f
BimoSora <bimosora3@gmail.com>
2021-08-06 08:21:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1311,7 +1311,7 @@ bot.on('video', async(ctx) => {
                     }
                     saver.saveFile(fileDetails3)
                     if(ctx.chat.type == 'private') {
-                       if(ctx.message.media_group_id){
+                       //if(ctx.message.media_group_id){
                           if(update.length > 1){
                               ctx.reply(`<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${ctx.message.media_group_id}`,{
                               parse_mode: 'HTML',
@@ -1320,7 +1320,7 @@ bot.on('video', async(ctx) => {
                           })
                           update = []
                           }
-                       }
+                       //}
                     }
                     if(!ctx.message.caption)
                     return ctx.telegram.sendMediaGroup(process.env.LOG_CHANNEL,[{
