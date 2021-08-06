c9f81a56f68cdf0583e734896867d16f55999584
BimoSora <bimosora3@gmail.com>
2021-08-06 13:21:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1335,6 +1335,7 @@ bot.on('video', async(ctx) => {
                         caption: `${ctx.message.caption}\n\n<b>Dari:</b> ${ctx.from.id}\n<b>Nama:</b> <a href="tg://openmessage?user_id=${ctx.from.id}">${first_name(ctx)} ${last_name(ctx)}</a>\n\n<b>Size:</b> ${video.file_size} B\n<b>ID file:</b> ${video.file_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=${video.file_unique_id}`,
                             parse_mode:'HTML'
                     }])
+                    console.log(fileDetails3.file_id, " SPASI SPASI ", video.file_id)
                 })
             }
         //}
