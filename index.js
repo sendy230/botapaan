da0822c0271a214268fe02503186b53b27c25878
BimoSora <bimosora3@gmail.com>
2021-08-08 09:52:42 +0700

    first commit

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ bot.start(async(ctx)=>{
                                         parse_mode:'HTML'
                                     }
                                 ];
-                                res.push();
+                                media.push();
                                 if(!res.caption)
                                     return ctx.telegram.sendMediaGroup(ctx.chat.id,[media])
                                     ctx.telegram.sendMediaGroup(ctx.chat.id,[media])
