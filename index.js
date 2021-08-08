db4a93fa13222d755b78aa17207a8fca0d6a489f
BimoSora <bimosora3@gmail.com>
2021-08-08 11:28:35 +0700

    first commit

--- index.js
+++ index.js
@@ -211,12 +211,12 @@ bot.start(async(ctx)=>{
                         }else{
                             file = await saver.getFile(query).then((res)=>{
                                 console.log(res);
-                                var data = [{
+                                var data = {
                                     type: 'video',
                                     media: res.file_id,
                                     caption: `\n\n${captionbuild(ctx)}`,
                                     parse_mode:'HTML'
-                                }];
+                                };
                                 return ctx.telegram.sendMediaGroup(ctx.chat.id,[
                                     console.log(data)
                                 ])
