ea34cc8a62e08df2296a13e1326bf4663b1ac824
BimoSora <bimosora3@gmail.com>
2021-08-08 09:40:27 +0700

    first commit

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ bot.start(async(ctx)=>{
                         }else{
                             file = await saver.getFile(query).then((res)=>{
                                 console.log(res);
-                                let res = [
+                                let query = [
                                     {
                                         type: 'video',
                                         media: res.file_id,
@@ -219,11 +219,11 @@ bot.start(async(ctx)=>{
                                         parse_mode:'HTML'
                                     }
                                 ];
-                                res.push();
-                                console.log(`Hasil ${res}`);
-                                if(!res.caption)
-                                    return ctx.telegram.sendMediaGroup(ctx.chat.id,[res])
-                                    ctx.telegram.sendMediaGroup(ctx.chat.id,[res])
+                                query.push();
+                                console.log(`Hasil ${query}`);
+                                if(!query.caption)
+                                    return ctx.telegram.sendMediaGroup(ctx.chat.id,[query])
+                                    ctx.telegram.sendMediaGroup(ctx.chat.id,[query])
                             })
                         }
                     }
