0398a42fe4bde584ad87e9e4ac788cc69381c7f2
BimoSora <bimosora3@gmail.com>
2021-08-08 12:28:01 +0700

    first commit

--- index.js
+++ index.js
@@ -211,6 +211,7 @@ bot.start(async(ctx)=>{
                         }else{
                             file = await saver.getFile(query).then((res)=>{
                                 //console.log(res);
+                                media =[]
                                 a = InputMediaVideo(
                                     media: res.file_id,
                                     caption: `\n\n${captionbuild(ctx)}`,
