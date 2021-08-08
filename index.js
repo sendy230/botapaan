b8b04295e7ea8ead14e5b553b3b6703cc91697be
BimoSora <bimosora3@gmail.com>
2021-08-08 09:35:49 +0700

    first commit

--- index.js
+++ index.js
@@ -211,7 +211,7 @@ bot.start(async(ctx)=>{
                         }else{
                             file = await saver.getFile(query).then((res)=>{
                                 console.log(res);
-                                let res = [
+                                var res = [
                                     {
                                         type: 'video',
                                         media: res.file_id,
