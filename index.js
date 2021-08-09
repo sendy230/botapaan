5b7e9258ae753b1a8fdcbfad87db399f55de9cc7
BimoSora <bimosora3@gmail.com>
2021-08-10 04:29:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1317,11 +1317,11 @@ bot.on('video', async(ctx) => {
     video = ctx.message.video
     //console.log(ctx);
 
-ctx2 = [video];
+                    //ctx2 = [video];
                     let mediagroup2 = [];
-                    for (let index2 = 0; index2 < ctx.length; index2++) {
-                         const data = ctx[index2];
-                         mediagroup2.push(data);
+                    for (let index2 = 0; index2 < video.length; index2++) {
+                         const data = video[index2];
+                         mediagroup2.push([data]);
                     }
 
                     console.log(mediagroup2);
