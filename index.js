b57cdfddebd4434b3aea8675bc299afd3cf902cd
BimoSora <bimosora3@gmail.com>
2021-08-09 11:45:17 +0700

    first commit

--- index.js
+++ index.js
@@ -988,7 +988,7 @@ bot.command('unbanchat', (ctx) => {
 })
 
 //list update
-let updates = []
+var updates = []
 bot.use(async (ctx, next) => {
     if(ctx.message.media_group_id){
        updates.push(ctx.update);
@@ -1435,7 +1435,7 @@ bot.on('video', async(ctx) => {
             saver.saveFile(fileDetails4)
             if(ctx.chat.type == 'private') {
                 if(ctx.message.media_group_id){
-                    if(updates.length > 2){
+                    if(updates.length > 1){
                         ctx.reply(`✔️ Grup disimpan \n<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=grp_${ctx.message.media_group_id}`,{
                             parse_mode: 'HTML',
                             disable_web_page_preview: true,
@@ -1463,7 +1463,7 @@ bot.on('video', async(ctx) => {
             saver.saveFile(fileDetails3)
             if(ctx.chat.type == 'private') {
                 if(ctx.message.media_group_id){
-                    if(updates.length > 2){
+                    if(updates.length > 1){
                         ctx.reply(`✔️ Grup disimpan \n<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=grp_${ctx.message.media_group_id}`,{
                             parse_mode: 'HTML',
                             disable_web_page_preview: true,
@@ -1565,7 +1565,7 @@ bot.on('video', async(ctx) => {
                     saver.saveFile(fileDetails4)
                     if(ctx.chat.type == 'private') {
                         if(ctx.message.media_group_id){
-                            if(updates.length > 0){
+                            if(updates.length > 1){
                                 ctx.reply(`✔️ Grup disimpan \n<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=grp_${ctx.message.media_group_id}`,{
                                     parse_mode: 'HTML',
                                     disable_web_page_preview: true,
@@ -1592,7 +1592,7 @@ bot.on('video', async(ctx) => {
                     saver.saveFile(fileDetails3)
                     if(ctx.chat.type == 'private') {
                         if(ctx.message.media_group_id){
-                            if(updates.length > 0){
+                            if(updates.length > 1){
                                 ctx.reply(`✔️ Grup disimpan \n<b>ID grup:</b> ${ctx.message.media_group_id}\n\nhttps://t.me/${process.env.BOTUSERNAME}?start=grp_${ctx.message.media_group_id}`,{
                                     parse_mode: 'HTML',
                                     disable_web_page_preview: true,
