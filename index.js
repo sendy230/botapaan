2b49458e6128dac2c8d27c0d553af70163770b1d
BimoSora <bimosora3@gmail.com>
2021-08-11 22:55:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,8 @@ function documentation(ctx){
 const inKey = [
   [{text:'🔎 Pencarian',switch_inline_query:''},{text:'📎 Tautan',callback_data:'POP'}],
   [{text:'📚 Dokumentasi',callback_data:'DOC'},{text:'🆘 Bantuan',callback_data:'HELP'}],
-  [{text:'📰 Informasi BOT', url: 'https://t.me/ratufilesaver'},{text:'📎 Gabung', url: `${process.env.LINKCHANNEL}`}]
+  [{text:'📰 Informasi BOT', url: 'https://t.me/ratufilesaver'}],
+  [{text:'📎 Gabung', url: `${process.env.LINKCHANNEL}`}]
 ];
 
 const inKey2 = [
