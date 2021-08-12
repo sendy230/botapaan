f4f2bcb8d9227dfc0b9e107e4826a2cd1b43d07d
BimoSora <bimosora3@gmail.com>
2021-08-12 15:55:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -85,11 +85,11 @@ const inKey = [
   [{text:'🔎 Pencarian',switch_inline_query:''},{text:'📎 Tautan',callback_data:'POP'}],
   [{text:'📚 Dokumentasi',callback_data:'DOC'},{text:'🆘 Bantuan',callback_data:'HELP'}],
   [{text:'📰 Informasi BOT', url: 'https://t.me/ratufilesaver'}],
-  [{text: url3, url: url4}]
+  [{text: `${url3}`, url: `${url4}`}]
 ];
 
 const inKey2 = [
-  [{text: url3, url: url4}]
+  [{text: `${url3}`, url: `${url4}`}]
 ];
 
 //BOT START
